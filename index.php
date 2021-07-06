0211cf3fec02676fd05499e40932b67e1abd198a
Stefania9797 <stefaniamastrel@gmail.com>
2021-07-06 15:22:53 +0200

    add query

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
 <?php
+// define
+define('DB_SERVERNAME', 'localhost');
+define("DB_USERNAME", 'root');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'university');
+
+//create sqli
+$conn = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection 
+if ($conn && $conn->connect_error) {
+    echo "Connection failed: " . $conn->connect_error;
+} else {
+    echo "Connection succesful";
+}
+//query
+$sql = "SELECT * FROM `students` WHERE YEAR(date_of_birth) = 1990";
+$result = $conn->query($sql);
+if ($result && $result->num_rows > 0) { ?>
+<h1>Students born in 1990</h1>
+<?php
+while($row = $result->fetch_assoc()) { ?>
+
+<?php }
+} elseif ($result) {
+    echo "0 results";
+} else {
+    echo "query error";
+}
+$conn->close();
+?>
